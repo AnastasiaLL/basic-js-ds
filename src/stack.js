7d4d93cb50941ceaf44c81b55cf457e71f2afc4c
AnastasiaLL <lukyanchik.nastya@bk.ru>
2022-05-02 01:17:22 +0300

    feat: add stack and queue

--- src/stack.js
+++ src/stack.js
@@ -12,21 +12,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
-class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+ class Stack {
+	constructor() {
+		this.stack = [];
+	}
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	push(element) {
+		return this.stack.push(element);
+	}
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	pop() {
+		return this.stack.pop()
+	}
+
+	peek() {
+		let lastElem = this.stack.pop();
+		this.stack.push(lastElem);
+		return lastElem
+	}
 }
 
 module.exports = {
